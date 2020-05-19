a9471e1424a16647d2a81d858d47d2f5206868f9
vagrant <vagrant@worker1>
2020-05-19 18:11:54 +0000

    Skeleton for sc2 game player. Need storage service & map generator

--- src/GameService/GameManager.py
+++ src/GameService/GameManager.py
@@ -4,12 +4,19 @@ from tournamentManager.TournamentManager import HttpTournamentManager
 from GameService.GamePlayer import MockGamePlayer
 
 tournament_manager = HttpTournamentManager()
+storage_service = None
 game_player = MockGamePlayer()
 
 @app.task
 def play_game(match, game):
-    result = game_player.play(game)
-    tournament_manager.report_match(match, result)
+    storage_service.get(game["participant1"]["script"])
+    storage_service.get(game["participant2"]["script"])
+    response = game_player.play(game["participant1"], game["participant2"], game["map"])
+    replay_url = storage_service.put(response["replay"])
+    game["winner"] = response["result"]
+    game["replayURL"] = replay_url
+    tournament_manager.report_match(match, game)
+    # TODO: delete scripts from filesystem
 
 def game_from_match(match):
     game = {
--- src/GameService/GamePlayer.py
+++ src/GameService/GamePlayer.py
@@ -1,26 +1,63 @@
 from abc import ABC, abstractmethod
 from random import randint
-from time import sleep
+import time
+import importlib
+
+import sc2
+from sc2 import run_game, maps, Race, Result
+from sc2.player import Bot
 
 class AbstractGamePlayer(ABC):
 
     @abstractmethod
-    def play(self, game):
+    def play(self, bot1, bot2, sc2_map):
         pass
 
 
 class StarcraftGamePlayer(AbstractGamePlayer):
 
-    def play(self, game):
-        # TODO: execute SC2 game
-        pass
+    def play(self, bot1, bot2, sc2_map):
+        Bot1 = getattr(importlib.import_module(bot1.name, '.'), bot1.name)
+        Bot2 = getattr(importlib.import_module(bot2.name, '.'), bot2.name)
+        replay_name = bot1.name + bot2.name + str(round(time.time() * 1000))
+        result = run_game(maps.get(sc2_map), [
+                            Bot(self._select_race(bot1.race), Bot1(), bot1.name),
+                            Bot(self._select_race(bot2.race), Bot2(), bot2.name),
+                        ], realtime=False, save_replay_as=replay_name)
+        return {
+            "result": result,
+            "replay": replay_name
+        }
+
+
+    def _parse_result(self, result):
+        if result is not None:
+            if result[0] == Result.Victory:
+                return 0
+            elif result[1] == Result.Victory:
+                return 1
+            elif result[0] == result[1] == Result.Tie:
+                return "draw"
+            else:
+                raise Exception("Invalid result, got " + str(result))
+        else:
+            raise Exception("Result is None")
+
+    def _select_race(self, race):
+        if race == 'Terran':
+            return Race.Terran
+        elif race == 'Protoss':
+            return Race.Protoss
+        elif race == 'Zerg':
+            return Race.Zerg
+        else:
+            raise Exception("Invalid race: expected 'Terran', 'Protoss' or 'Zerg', got " + str(race))
 
 class MockGamePlayer(AbstractGamePlayer):
 
-    def play(self, game):
-        winner = randint(0, 1)
-        game["winner"] = winner
-        game["map"] = "mockMap"
-        game["replayURL"] = "mockReplay"
-        sleep(20)
-        return game
+    def play(self, bot1, bot2, sc2_map):
+        time.sleep(20)
+        return {
+            "result": randint(0, 1),
+            "replay": "mockReplay"
+        }
